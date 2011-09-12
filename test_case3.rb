766b4898b0e4557b2e5c40a0bae597eb857a32ec
David Bates <me@dave-bates.net>
2011-09-12 13:57:52 -0500

    Cleaned up the test file and changed the line endings

--- test_case3.rb
+++ test_case3.rb
@@ -6,46 +6,23 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
 require 'api'
 
 api = API::OrdrIn.new('mlJhC8iX4BGWVtn','https://r-test.ordr.in')
-# api = API::OrdrIn.new('','')
 dt = API::DT.new
-dt.asap = true;
-
 st =  API::Money.new(500.00)
 tip =  API::Money.new(st.amount * 0.15)
-
-api.set_curr_acct('dave@batez-consulting.com','password')
-# puts api
-# puts dt
-
 a = API::Address.new('street with spaces', 'Cedar Rapids', '52404', 'street2','IA', '319-366-2309','nick')
-# puts a.to_s
-
-# puts "Starting Validation - Address"
-a.validate
-# puts "\nEnding Validation - Address"
-# puts a
-
 m = API::Money.new(23.44)
-# puts m
+
+dt.asap = true;
+api.set_curr_acct('dave@batez-consulting.com','password')
 
 r = API::Restaurant.new
-# r.to_s
-# r.delivery_check(33, dt, a)
-# r.delivery_fee(33, st, tip, dt, a)
-# r.details(33)
+$_url = 'https://r-test.ordr.in'
 
 o = API::Order.new
 $_url = 'https://o-test.ordr.in'
 o.submit(33, 'tray1', tip, dt, 'dabates77@gmail.com', 'David', 'Bates', a, 'Discover', '6011000990139424', '040', '052011', a)
 
 u = API::User.new
-# puts api
+$_url = 'https://u-test.ordr.in'
 
-# u.update_address(a)
-# u.get_card
-# u.get_card('DISC')
-# u.update_password('test2')
-# $_url = 'https://u-test.ordr.in'
-# u.get_acct
-
-$_errors.map {|e| puts 'Error : ' + e} if !$_errors.empty?
+$_errors.map {|e| puts 'Error : ' + e} unless $_errors.empty?
