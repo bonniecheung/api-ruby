bcb177975a4ebbc551a952af313ed09b554e3881
David Bates <dave@batez-consulting.com>
2011-09-12 20:44:34 -0500

    Removed development files

--- OrdrIn/Money.rb
+++
@@ -1,8 +0,0 @@
-#
-# Order.in Ruby Library Alpha
-# http://ordr.in
-# Money Module
-#
-# (c) 2011
-# Last update: September 2011
-#
--- OrdrIn/User.rb
+++
@@ -1,8 +0,0 @@
-#
-# Order.in Ruby Library Alpha
-# http://ordr.in
-# USER API
-#
-# (c) 2011
-# Last update: September 2011
-#
